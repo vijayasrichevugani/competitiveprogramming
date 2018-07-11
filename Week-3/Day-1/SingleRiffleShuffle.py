b112af27e39aef23f152f5ca0d8b9efe1d90bd0f
vijayasrichevugani <34120632+vijayasrichevugani@users.noreply.github.com>
2018-07-11 10:12:14 +0530

    Create SingleRiffleShuffle.py

---
+++ Week-3/Day-1/SingleRiffleShuffle.py
@@ -0,0 +1,40 @@
+import unittest
+
+def is_single_riffle(half1, half2, shuffled_deck):
+    h1 = 0
+    h2 = 0
+    for card in shuffled_deck:
+        if h1 < len(half1) and card == half1[h1]:
+            h1 += 1
+        elif h2 < len(half2) and card == half2[h2]:
+            h2 += 1
+        else:
+            return False
+    return True
+
+# Test Cases
+
+class Test(unittest.TestCase):
+
+    def test_both_halves_are_the_same_length(self):
+        result = is_single_riffle([1, 4, 5], [2, 3, 6], [1, 2, 3, 4, 5, 6])
+        self.assertTrue(result)
+
+    def test_halves_are_different_lengths(self):
+        result = is_single_riffle([1, 5], [2, 3, 6], [1, 2, 6, 3, 5])
+        self.assertFalse(result)
+
+    def test_one_half_is_empty(self):
+        result = is_single_riffle([], [2, 3, 6], [2, 3, 6])
+        self.assertTrue(result)
+
+    def test_shuffled_deck_is_missing_cards(self):
+        result = is_single_riffle([1, 5], [2, 3, 6], [1, 6, 3, 5])
+        self.assertFalse(result)
+
+    def test_shuffled_deck_has_extra_cards(self):
+        result = is_single_riffle([1, 5], [2, 3, 6], [1, 2, 3, 5, 6, 8])
+        self.assertFalse(result)
+
+
+unittest.main(verbosity=2)
