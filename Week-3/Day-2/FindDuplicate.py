b5faf99f1e7ad2b9b67a2361a0c58c8e5693c439
vijayasrichevugani <34120632+vijayasrichevugani@users.noreply.github.com>
2018-07-11 10:13:14 +0530

    Create FindDuplicate.py

---
+++ Week-3/Day-2/FindDuplicate.py
@@ -0,0 +1,35 @@
+import unittest
+
+def find_repeat(arr):
+    # Find a number that appears more than once
+    for i in range(0, len(arr)):
+        if arr[abs(arr[i])] >= 0:
+            arr[abs(arr[i])] = -arr[abs(arr[i])]
+        else:
+            return abs(arr[i])
+# Test Cases
+
+class Test(unittest.TestCase):
+
+    def test_just_the_repeated_number(self):
+        actual = find_repeat([1, 1])
+        expected = 1
+        self.assertEqual(actual, expected)
+
+    def test_short_list(self):
+        actual = find_repeat([1, 2, 3, 2])
+        expected = 2
+        self.assertEqual(actual, expected)
+
+    def test_medium_list(self):
+        actual = find_repeat([1, 2, 5, 5, 5, 5])
+        expected = 5
+        self.assertEqual(actual, expected)
+
+    def test_long_list(self):
+        actual = find_repeat([4, 1, 4, 8, 3, 2, 7, 6, 5])
+        expected = 4
+        self.assertEqual(actual, expected)
+
+
+unittest.main(verbosity=2)
