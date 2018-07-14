e457f055459d2b07a3c6fa6e5ff5ec8f143e110d
vijayasrichevugani <34120632+vijayasrichevugani@users.noreply.github.com>
2018-07-14 14:30:33 +0530

    Update HammingDistance.py

--- Week-3/Exam/HammingDistance.py
+++ Week-3/Exam/HammingDistance.py
@@ -1,10 +1,10 @@
 def main(): 
     def hammingDistance(x, y): 
-        res = 0 
-        for c in str(bin(x ^ y)): 
-            if c == '1': 
-                res += 1 
-        return res 
+        result = 0 
+        for i in str(bin(x ^ y)): 
+            if i == '1': 
+                result += 1 
+        return result 
     print hammingDistance(25, 30) 
     print hammingDistance(1, 4) 
     print hammingDistance(25, 30) 
